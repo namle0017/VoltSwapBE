13d14fee711af540ee51646e1f087e4c34b4a7b0
Namle <79929679+namle0017@users.noreply.github.com>
2025-11-03 09:43:29 +0700

    Add files via upload

---
+++ VoltSwap.Common/DTOs/FeeDtos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoltSwap.Common.DTOs
+{
+    public class FeeDtos
+    {
+        public class UpdateFeeGroupRequest
+        {
+            public string GroupKey { get; set; } = string.Empty;
+            public List<FeeUpdateRequest> Fees { get; set; } = new();
+        }
+
+        public class FeeUpdateRequest
+        {
+            public string TypeOfFee { get; set; } = string.Empty;
+            public decimal? Amount { get; set; }
+            public string? Unit { get; set; }
+            public List<ExcessMileageTierDto>? Tiers { get; set; } 
+        }
+
+        public class ExcessMileageTierDto
+        {
+            public decimal MinValue { get; set; }
+            public decimal MaxValue { get; set; }
+            public decimal Amount { get; set; }
+            public string Unit { get; set; } = string.Empty;
+        }
+
+    }
+}
